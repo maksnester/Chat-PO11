c8b960d0fa11ade2f5c5f7557cab3d65baaadc15
qRoman <romanalipay@gmail.com>
2015-03-14 23:33:49 +1100

    Добавлены сессии

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var User = require('models/user').User;
-var HttpError = require('error').HttpError;
+var User = require('../models/user').User;
+var HttpError = require('../error').HttpError;
 var ObjactID = require('mongodb').ObjectID;
 
 module.exports = function(app) {
